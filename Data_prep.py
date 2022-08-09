4c948cd41bff91d7a8723050e2a94c5542d2303d
oraby14 <104243748+oraby14@users.noreply.github.com>
2022-08-09 02:21:43 +0200

    Add files via upload

---
+++ Data_prep.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import re
+import numpy as np
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.preprocessing import Normalizer
+import pandas as pd
+
+
+def question_preprocessing(text):
+    stopwords= ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've",            "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself',             'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their',            'theirs', 'themselves', 'this', 'that', "that'll", 'these', 'those',             'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does',             'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of',             'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after',            'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further',            'then', 'once', 'here', 'there', 'all', 'any', 'both', 'each', 'few', 'more',            'most', 'other', 'some', 'such', 'only', 'own', 'same', 'so', 'than', 'too', 'very',             's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're',             've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn',            "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn',            "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't",             'won', "won't", 'wouldn', "wouldn't"]
+    
+
+    preprocessed_questions = []
+    for sentance in text[0].values:
+        sent = decontracted(sentance)
+        sent = sent.replace('\\r', ' ')
+        sent = sent.replace('\\"', ' ')
+        sent = sent.replace('\\n', ' ')
+        sent = re.sub('[^A-Za-z0-9]+', ' ', sent)
+        sent = sent.lower()
+        sent = ' '.join(e for e in sent.split() if e not in stopwords)
+        preprocessed_questions.append(sent.lower().strip())
+    
+    docs = preprocessed_questions
+    t = Tokenizer()
+    t.fit_on_texts(docs)
+    vocab_size = len(t.word_index) + 1
+    encoded_docs = t.texts_to_sequences(docs)
+    X_padded_ques = pad_sequences(encoded_docs, maxlen=800, padding='post')
+    normalizer = Normalizer()
+    normalizer.fit(X_padded_ques)
+
+    stopwords= ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've",            "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself',             'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their',            'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those',             'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does',             'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of',             'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after',            'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further',            'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',            'most', 'other', 'some', 'such', 'only', 'own', 'same', 'so', 'than', 'too', 'very',             's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're',             've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn',            "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn',            "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't",             'won', "won't", 'wouldn', "wouldn't"]
+    
+    preprocessed_ans = []
+    for sentance in text['1'].values:
+        sent = decontracted(sentance)
+        sent = sent.replace('\\r', ' ')
+        sent = sent.replace('\\"', ' ')
+        sent = sent.replace('\\n', ' ')
+        sent = re.sub('[^A-Za-z0-9]+', ' ', sent)
+        sent = sent.lower()
+        sent = ' '.join(e for e in sent.split() if e not in stopwords)
+        preprocessed_ans.append(sent.lower().strip())
+    
+    docs = preprocessed_ans
+    t = Tokenizer()
+    t.fit_on_texts(docs)
+    vocab_size = len(t.word_index) + 1
+    encoded_docs = t.texts_to_sequences(docs)
+    X_padded_ans = pad_sequences(encoded_docs, maxlen=800, padding='post')
+    normalizer = Normalizer()
+    normalizer.fit(X_padded_ans) 
+    return X_padded_ans ,  X_padded_ques
+    
+    
+    
+    
+    
+    
+    
+        
+def decontracted(phrase):
+    phrase = re.sub(r"won't", "will not", phrase)
+    phrase = re.sub(r"can\'t", "can not", phrase)
+    phrase = re.sub(r"n\'t", " not", phrase)
+    phrase = re.sub(r"\'re", " are", phrase)
+    phrase = re.sub(r"\'s", " is", phrase)
+    phrase = re.sub(r"\'d", " would", phrase)
+    phrase = re.sub(r"\'ll", " will", phrase)
+    phrase = re.sub(r"\'t", " not", phrase)
+    phrase = re.sub(r"\'ve", " have", phrase)
+    phrase = re.sub(r"\'m", " am", phrase)
+    return phrase
+
+
+# In[ ]:
+
+
+
+
